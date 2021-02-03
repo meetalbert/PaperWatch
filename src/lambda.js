9331ecfb712058d7e05e28b72d8c947b35e6ba7e
Nathan Hopper <nathan@albert.com>
2021-02-02 18:42:44 -0800

    Reuse papertrail transport (#1)
    * [fix] store papertrail transport at the global level
    
    Storing the transport objects at the global level enables them to be reused between lambda invocations.
    
    * store the transport globally

--- src/lambda.js
+++ src/lambda.js
@@ -4,6 +4,8 @@ var logger = require('./lib/logger');
 var winston = require('winston');
 require('winston-papertrail').Papertrail;
 
+var transports = {};
+
 // Handler function
 exports.handler = function(event, context, callback){
 
@@ -12,17 +14,32 @@ exports.handler = function(event, context, callback){
     if(err)
       return callback(err);
 
-    // Construct the winston transport for forwarding lambda logs to papertrail
-    var papertrail = new winston.transports.Papertrail({
-      level: 'silly',
-      host: config.host,
-      port: config.port,
-      hostname: "Lambda_" + data.owner + "_" + process.env.AWS_REGION,
-      program: data.logGroup.split('/').pop(),
-      logFormat: function(level, message){
-        return message;
-      }
-    });
+    var hostname = "Lambda_" + data.owner + "_" + process.env.AWS_REGION;
+    var program = data.logGroup.split('/').pop();
+
+    // Build a dictionary of hostname and programs
+    if(!(hostname in transports)){
+      transports[hostname] = {};
+    }
+
+    var papertrail;
+    if(program in transports[hostname]){
+      papertrail = transports[hostname][program]
+    }else{
+      // Construct the winston transport for forwarding lambda logs to papertrail
+      papertrail = new winston.transports.Papertrail({
+        level: 'silly',
+        host: config.host,
+        port: config.port,
+        hostname: hostname,
+        program: program,
+        logFormat: function (level, message){
+          return message;
+        }
+      });
+      // Store the transport globally
+      transports[hostname][program] = papertrail
+    }
     // post the logs
     logger.post(data, papertrail, callback);
   });
