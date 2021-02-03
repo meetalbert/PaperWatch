1c82ed57d7c608f4e2c58e9f9509d7151b1e9ce0
Nathan Hopper <nathan@albert.com>
2021-02-02 18:13:45 -0800

    [fix] store papertrail transport at the global level
    Storing the transport objects at the global level enables them to be reused between lambda invocations.

--- src/apiGateway.js
+++ src/apiGateway.js
@@ -7,6 +7,7 @@ var winston = require('winston');
 require('winston-papertrail').Papertrail;
 
 var restApis;
+var transports = {};
 
 // Handler function
 exports.handler = function(event, context, callback){
@@ -44,6 +45,7 @@ function constructTransport(event, callback){
 
     // Construct the program name from the log group name
     var program = data.logGroup.split("_").splice(1).join("_").split("/");
+    var hostname = "API-Gateway_" + data.owner + "_" + process.env.AWS_REGION;
 
     // If possible, replace the api ID with it's name
     if(restApis && restApis[program[0]])
@@ -51,16 +53,26 @@ function constructTransport(event, callback){
 
     program = program.join("_");
 
-    // Construct the winston transport for forwarding lambda logs to papertrail
-    var papertrail = new winston.transports.Papertrail({
-      host: config.host,
-      port: config.port,
-      hostname: "API-Gateway_" + data.owner + "_" + process.env.AWS_REGION,
-      program: program,
-      logFormat: function(level, message){
-        return message;
-      }
-    });
+    if(!(hostname in transports)){
+      transports[hostname] = {};
+    }
+
+    var papertrail;
+    if(program in transports[hostname]){
+      papertrail = transports[hostname][program]
+    }else{
+      // Construct the winston transport for forwarding lambda logs to papertrail
+      papertrail = new winston.transports.Papertrail({
+        host: config.host,
+        port: config.port,
+        hostname: hostname,
+        program: program,
+        logFormat: function(level, message){
+          return message;
+        }
+      });
+    }
+
     // post the logs
     logger.post(data, papertrail, callback);
   });
